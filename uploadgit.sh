664669238e4393ea8c52c21000a50e52120e98cb
root <pengdannywang@gmail.com>
2019-05-29 13:36:17 +1000

    auto

---
+++ uploadgit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash  
+cd /root/pythondev/JanePython
+/usr/lib/git-core/git commit -a -m 'auto'
+/usr/lib/git-core/git push https://pengdannywang:Wang9643@github.com/pengdannywang/JanePython.git --all
+
